cab362249245a53f40039a4a871d66d7aafade3d
JairCologneseJunior <102156480+JairCologneseJunior@users.noreply.github.com>
2024-12-03 19:09:57 -0300

    Delete Estoque-front-main directory

--- Estoque-front-main/src/app/components/tela-inicio/tela-inicio.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tela-inicio',
-  standalone: false,
-  templateUrl: './tela-inicio.component.html',
-  styleUrl: './tela-inicio.component.css',
-})
-export class TelaInicioComponent {}
